d7d343a052eb74d9d366835403d8ff9544c8f491
ataimamatov <altair.mamatov@gmail.com>
2023-04-06 19:57:57 +0600

    Global Client Controller (Create,Edit,Delete) in one

---
+++ ClientsSite.API/Controllers/ClientController.cs
@@ -0,0 +1,56 @@
+﻿using ClientsSite.Data.Request;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClientsSite.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ClientController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<int>> CreateClient(CreateClientRequest request)
+        {
+            var clientId = await _mediator.Send(request);
+            return Ok(clientId);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditClient(int id, EditClientRequest request)
+        {
+            if (id != request.Id)
+            {
+                return BadRequest();
+            }
+
+            var result = await _mediator.Send(request);
+
+            if (result == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<int>> DeleteClient(int id)
+        {
+            var request = new DeleteClientRequest { Id = id };
+            var clientId = await _mediator.Send(request);
+            return Ok(clientId);
+        }
+
+
+    }
+}
+
--- ClientsSite.API/Program.cs
+++ ClientsSite.API/Program.cs
@@ -12,9 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
 builder.Services.AddDbContext<AppDbContext>();
 builder.Services.AddControllers();
-builder.Services.AddTransient<CreateClientController>();
-builder.Services.AddTransient<EditClientController>();
-builder.Services.AddTransient<DeleteClientController>();
+builder.Services.AddTransient<ClientController>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
